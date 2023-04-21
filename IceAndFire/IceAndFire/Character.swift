eceaabab8749ca9e527e8d44e5f84685e784904e
Martin Klöpfel <martin.kloepfel-extern@deutschebahn.com>
2023-04-21 21:04:19 +0200

    Add `Character` entity

---
+++ IceAndFire/IceAndFire/Character.swift
@@ -0,0 +1,31 @@
+//
+//  Character.swift
+//  IceAndFire
+//
+//  Created by Martin Klöpfel on 21.04.23.
+//
+
+import Foundation
+
+struct Character: Decodable {
+    
+    /// The hypermedia URL of this resource
+    let url: URL?
+    
+    /// The name of this character
+    let name: String
+    
+    /// The gender of this character.
+    let gender: String
+    
+    /// Textual representation of when and where this character died.
+    let died: String
+    
+    /// The aliases that this character goes by.
+    let aliases: [String]
+    
+    /// An array of actor names that has played this character in the TV show Game Of Thrones.
+    let playedBy: [String]
+}
+
+extension Character: Equatable {}
