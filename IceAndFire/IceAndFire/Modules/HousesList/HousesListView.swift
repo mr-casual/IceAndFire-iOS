98e6cd9924b744d9b0f14b67fa45711e8b2f2966
Martin Klöpfel <martin.kloepfel-extern@deutschebahn.com>
2023-04-25 18:06:21 +0200

    Fix loading view when paging

--- IceAndFire/IceAndFire/Modules/HousesList/HousesListView.swift
+++ IceAndFire/IceAndFire/Modules/HousesList/HousesListView.swift
@@ -10,35 +10,34 @@ import SwiftUI
 struct HousesListView: View {
     
     @StateObject var viewModel: HousesListViewModel
-
+    
     var body: some View {
         List {
-            ForEach(viewModel.houses, id: \.self) { house in
-                NavigationLink {
-                    HouseDetailView(viewModel: .init(house: house,
-                                                     service: viewModel.service))
-                } label: {
-                    Text(house.name)
-                }
-            }
-            
-            if viewModel.isLoading {
-                HStack {
-                    Spacer()
-                    ProgressView()
-                        .padding(16)
-                    Spacer()
-                }
-            } else if let errorText = viewModel.errorText {
-                ErrorView(errorText: errorText) {
-                    Task { viewModel.loadMore }
-                }
-            } else if viewModel.isMoreAvailable {
-                Color.clear
-                    .frame(height: 1)
-                    .onAppear {
-                        Task { await viewModel.loadMore() }
+            Section {
+                ForEach(viewModel.houses, id: \.self) { house in
+                    NavigationLink {
+                        HouseDetailView(viewModel: .init(house: house,
+                                                         service: viewModel.service))
+                    } label: {
+                        Text(house.name)
                     }
+                }
+            } footer: {
+                if viewModel.isLoading {
+                    ProgressView()
+                        .alignCentered()
+                        .padding(16)
+                } else if let errorText = viewModel.errorText {
+                    ErrorView(errorText: errorText) {
+                        Task { viewModel.loadMore }
+                    }
+                } else if viewModel.isMoreAvailable {
+                    Color.clear
+                        .frame(height: 1)
+                        .onAppear {
+                            Task { await viewModel.loadMore() }
+                        }
+                }
             }
         }
         .navigationTitle("Houses of Ice and Fire")
